83632efd637c3c1bbbd3fc9e5b32ed11ce907ee0
Ivan Lysov <ivanlysov@MacBook-Pro-Ivan.local>
2024-05-23 19:10:05 +0500

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <SafeAreaView className="h-full">
+      <View className="h-full items-center justify-center">
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
