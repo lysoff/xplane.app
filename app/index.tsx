df1e894cfe3f74e637b58056930e0f264a5c5c20
Ivan Lysov <ivanlysov@MacBook-Pro-Ivan.local>
2024-05-24 11:16:39 +0500

    initial theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,17 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 const Home = () => {
   return (
-    <SafeAreaView className="h-full">
-      <View className="h-full items-center justify-center">
-        <Text>Home</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView className="h-full bg-primary">
+        <View className="h-full items-center justify-center bg-primary">
+          <Text className="text-xl font-pregular text-secondary">Home</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="light" />
+    </>
   );
 };
 
