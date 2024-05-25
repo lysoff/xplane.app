bb915a28f4f5993ae2d073cbf9b9def45f797e73
Ivan Lysov <ivan.lysov@gmail.com>
2024-05-25 17:32:05 +0500

    @ path alias

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,9 +1,16 @@
-import { View, Text, SafeAreaView, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import Button from "../../components/Button";
 import { Link, router } from "expo-router";
-import { Icon } from "../../constants/icons";
+import { Icon } from "@/constants/icons";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -21,65 +28,97 @@ const SignUp = () => {
   return (
     <>
       <SafeAreaView className="h-full bg-primary  items-center justify-center">
-        <View className="h-full items-center justify-center bg-primary w-[300px]">
-          <Text className="text-xl font-pregular text-secondary">
-            Welcome to XPlane
-          </Text>
+        <ScrollView
+          contentContainerStyle={{
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <KeyboardAvoidingView behavior="padding">
+            <View className="h-full items-center justify-center bg-primary w-[300px]">
+              <Text className="text-xl font-pregular text-secondary">
+                Welcome to XPlane
+              </Text>
 
-          <View className="flex-col gap-y-2 my-2 w-full">
-            <Text className="text-gray-500 text-xl">Email</Text>
-            <TextInput
-              value={form.email}
-              onChangeText={(e) => setForm({ ...form, email: e })}
-              className="text-white text-xl rounded-xl border-2 p-5 border-solid border-gray-700 w-full"
-            />
-          </View>
+              <View className="flex-col gap-y-2 my-2 w-full">
+                <Text className="text-gray-500 text-xl">Email</Text>
+                <TextInput
+                  value={form.email}
+                  onChangeText={(e) => setForm({ ...form, email: e })}
+                  className="text-white text-xl rounded-xl border-2 p-5 border-solid border-gray-700 w-full"
+                />
+              </View>
 
-          <View className="flex-col gap-y-2 my-2 w-full">
-            <Text className="text-gray-500 text-xl">Password</Text>
-            <View className="flex-row pr-2 items-center rounded-xl border-2 border-solid border-gray-700 w-full">
-              <TextInput
-                value={form.password}
-                onChangeText={(e) => setForm({ ...form, password: e })}
-                secureTextEntry={!showPassword}
-                className="flex-1 p-5 mr-2 text-white text-xl "
-              />
-              <Icon
-                onPress={() => setShowPassword(!showPassword)}
-                name={!showPassword ? "eye" : "eye-off"}
-                size={24}
-                color="#666"
-              />
+              <View className="flex-col gap-y-2 my-2 w-full">
+                <Text className="text-gray-500 text-xl">Password</Text>
+                <View className="flex-row pr-2 items-center rounded-xl border-2 border-solid border-gray-700 w-full">
+                  <TextInput
+                    value={form.password}
+                    onChangeText={(e) => setForm({ ...form, password: e })}
+                    secureTextEntry={!showPassword}
+                    className="flex-1 p-5 mr-2 text-white text-xl "
+                  />
+                  <Icon
+                    onPress={() => setShowPassword(!showPassword)}
+                    name={!showPassword ? "eye" : "eye-off"}
+                    size={24}
+                    color="#666"
+                  />
+                </View>
+              </View>
+
+              <View className="flex-col gap-y-2 my-2 w-full">
+                <Text className="text-gray-500 text-xl">Confirm Password:</Text>
+                <View className="flex-row pr-2 items-center rounded-xl border-2 border-solid border-gray-700 w-full">
+                  <TextInput
+                    value={form.confirmPassword}
+                    onChangeText={(e) =>
+                      setForm({ ...form, confirmPassword: e })
+                    }
+                    secureTextEntry={!showConfirmPassword}
+                    className="flex-1 p-5 mr-2 text-white text-xl "
+                  />
+                  <Icon
+                    onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+                    name={!showConfirmPassword ? "eye" : "eye-off"}
+                    size={24}
+                    color="#666"
+                  />
+                </View>
+              </View>
+
+              <View className="flex-col gap-y-2 my-2 w-full">
+                <Text className="text-gray-500 text-xl">Confirm Password:</Text>
+                <View className="flex-row pr-2 items-center rounded-xl border-2 border-solid border-gray-700 w-full">
+                  <TextInput
+                    value={form.confirmPassword}
+                    onChangeText={(e) =>
+                      setForm({ ...form, confirmPassword: e })
+                    }
+                    secureTextEntry={!showConfirmPassword}
+                    className="flex-1 p-5 mr-2 text-white text-xl "
+                  />
+                  <Icon
+                    onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+                    name={!showConfirmPassword ? "eye" : "eye-off"}
+                    size={24}
+                    color="#666"
+                  />
+                </View>
+              </View>
+
+              <View className="flex-row w-full py-5">
+                <Button containerStyles="mr-5" onPress={handleSubmit}>
+                  Sign Up
+                </Button>
+                <Link className="text-secondary flex-shrink" href="/signin">
+                  Already have an account? Sign in
+                </Link>
+              </View>
             </View>
-          </View>
-
-          <View className="flex-col gap-y-2 my-2 w-full">
-            <Text className="text-gray-500 text-xl">Confirm Password:</Text>
-            <View className="flex-row pr-2 items-center rounded-xl border-2 border-solid border-gray-700 w-full">
-              <TextInput
-                value={form.confirmPassword}
-                onChangeText={(e) => setForm({ ...form, confirmPassword: e })}
-                secureTextEntry={!showConfirmPassword}
-                className="flex-1 p-5 mr-2 text-white text-xl "
-              />
-              <Icon
-                onPress={() => setShowConfirmPassword(!showConfirmPassword)}
-                name={!showConfirmPassword ? "eye" : "eye-off"}
-                size={24}
-                color="#666"
-              />
-            </View>
-          </View>
-
-          <View className="flex-row w-full py-5">
-            <Button containerStyles="mr-5" onPress={handleSubmit}>
-              Sign Up
-            </Button>
-            <Link className="text-secondary flex-shrink" href="/signin">
-              Already have an account? Sign in
-            </Link>
-          </View>
-        </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
       </SafeAreaView>
       <StatusBar style="light" />
     </>
