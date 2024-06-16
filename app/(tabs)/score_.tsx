f0484082f417b8797c1038ba3457f9101f745f62
Ivan Lysov <ivan.lysov@gmail.com>
2024-06-16 12:22:13 +0500

    randomTimestamps; x-scale

--- app/(tabs)/score_.tsx
+++ app/(tabs)/score_.tsx
@@ -1,11 +1,26 @@
 import { SafeAreaView, Text } from "react-native";
 import React from "react";
 import { randomTimestamps } from "@/utils/randomTimestamps";
+import * as scale from "d3-scale";
+import { ConsoleColor } from "@/utils/utils";
 
 const Score = () => {
   const timestamps = randomTimestamps();
 
   console.log({ timestamps });
+
+  const x = scale
+    .scaleTime()
+    .range([0, 300])
+    .domain([timestamps.start, timestamps.end]);
+
+  console.log(x.ticks());
+
+  for (const value of timestamps.array) {
+    console.log(ConsoleColor.FgGreen, x(value));
+  }
+
+  console.log(x.invert(200));
 
   return (
     <SafeAreaView className="h-full bg-primary items-center">
--- utils/randomTimestamps.ts
+++ utils/randomTimestamps.ts
@@ -1,19 +1,25 @@
 import { DateTime } from "luxon";
 
 export const randomTimestamps = () => {
-  const start = DateTime.now().startOf("day").toMillis();
-  const end = DateTime.now().endOf("day").toMillis();
-  const diff = end - start;
+  const start = DateTime.now().startOf("day");
+  const end = DateTime.now().endOf("day");
+  const diff = end.toMillis() - start.toMillis();
 
   const array = [];
 
   for (let i = 0; i < 10; i++) {
-    array.push(start + Math.trunc(Math.random() * diff));
+    array.push(
+      DateTime.fromMillis(
+        start.toMillis() + Math.trunc(Math.random() * diff)
+      ).toJSDate()
+    );
   }
 
+  array.sort((a, b) => (a > b ? 1 : -1));
+
   return {
-    start,
-    end,
+    start: start.toJSDate(),
+    end: end.toJSDate(),
     diff,
     array,
   };
